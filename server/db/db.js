fcfd71acb59434f0c3e519e13770e35ded63b9b5
Jason Belcher <belcher.jason@gmail.com>
2019-10-17 13:00:27 -0400

    setup local db reference for development

--- server/db/db.js
+++ server/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 mongoose.set('debug', true);
 mongoose.Promise = Promise;
 
-mongoose.connect(process.env.MONGO_URI, (err) => {
+mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/devdb", (err) => {
     console.error(err);
 
 });
