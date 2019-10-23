cda2c809eb26010653913d15068da7bc67eef851
Jason Belcher <belcher.jason@gmail.com>
2019-10-23 13:13:40 -0400

    data aggregation and state management

--- frontend/turner-app-challenge/src/app/app.component.ts
+++ frontend/turner-app-challenge/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { TitlesService } from './titles.service';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,27 +10,44 @@ import { TitlesService } from './titles.service';
 })
 export class AppComponent implements OnInit {
   titles$: Observable<any[]>;
+  awards$: Observable<any[]>;
+  storylines$: Observable<any[]>;
+  participants$: Observable<any[]>;
   titleNames: string[];
   isOpen = false;
 
   selected: string;
 
-  constructor(private api: TitlesService) {}
+  constructor(private api: TitlesService) {
+    this.selected = '';
+  }
 
   ngOnInit() {
-    this.titles$ = this.api.getTitles('');
+    this.updateData();
     this.api.getTitleNames().then((names) => {
       this.titleNames = names;
     });
   }
 
+  updateData() {
+    this.api.getTitlesAggregatted(this.selected);
+    this.titles$ = this.api.titlesObs;
+    this.awards$ = this.api.awardsObs;
+    this.storylines$ = this.api.storylinesObs;
+    this.participants$ = this.api.participantsObs;
+  }
+
   getSearchTerm() {
     if (!this.selected) {
-      this.titles$ = this.api.getTitles(this.selected);
+      this.updateData();
       this.isOpen = false;
       this.selected = '';
     } else {
-      this.titles$ = this.api.getTitles(this.selected);
+      this.api.getTitlesAggregatted(this.selected);
+      this.titles$ = this.api.titlesObs;
+      this.awards$ = this.api.awardsObs;
+      this.storylines$ = this.api.storylinesObs;
+      this.participants$ = this.api.participantsObs;
       this.isOpen = true;
       this.selected = '';
     }
--- frontend/turner-app-challenge/src/app/titles.service.ts
+++ frontend/turner-app-challenge/src/app/titles.service.ts
@@ -1,19 +1,66 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpResponse, HttpEvent } from '@angular/common/http';
+import { Observable, BehaviorSubject } from 'rxjs';
+import {ITitle} from './interfaces/title.interface';
+import { IAwards } from './interfaces/awards.interface';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class TitlesService {
   private URL: string;
+  titlesRepository: {titles: ITitle[], awards: IAwards[], genres: IGenres[], storylines: any[], participants: any[]};
+  public titlesObs: Observable<any[]>;
+  public awardsObs: Observable<any[]>;
+  public storylinesObs: Observable<any[]>;
+  public participantsObs: Observable<any[]>;
+  private titlesBSubject: BehaviorSubject<any[]>;
+  private awardsBSubject: BehaviorSubject<any[]>;
+  private storylinesBSubject: BehaviorSubject<any[]>;
+  private participantsBSubject: BehaviorSubject<any[]>;
+
+
 
   constructor(private http: HttpClient) {
     this.URL = `http://localhost:8080/api/v1/titles`;
+    this.titlesRepository = {titles: [], awards: [], genres: [], storylines: [], participants: []};
+    this.titlesBSubject = new BehaviorSubject([]);
+    this.awardsBSubject = new BehaviorSubject([]);
+    this.storylinesBSubject = new BehaviorSubject([]);
+    this.participantsBSubject = new BehaviorSubject([]);
+    this.titlesObs = this.titlesBSubject.asObservable();
+    this.awardsObs = this.awardsBSubject.asObservable();
+    this.storylinesObs = this.storylinesBSubject.asObservable();
+    this.participantsObs = this.participantsBSubject.asObservable();
   }
 
-  getTitles(query: string): Observable<any[]> {
+  getTitles(query: string): Observable<ITitle[]> {
     return this.http.get<any[]>(`${this.URL}/?TitleName=${query}`);
+  }
+
+  getTitlesAggregatted(query: string): void {
+
+    this.http.get<any[]>(`${this.URL}/?TitleName=${query}`).subscribe((data: any) => {
+      this.titlesRepository.titles = data;
+      this.titlesRepository.awards = data.map(title => {
+        return {id: title._id, TitleName: title.TitleName, ReleaseYear: title.ReleaseYear, Awards: title.Awards };
+      });
+
+      this.titlesRepository.storylines = data.map(title => {
+        return {id: title._id, TitleName: title.TitleName, ReleaseYear: title.ReleaseYear, storylines: title.Storylines };
+      });
+
+      this.titlesRepository.participants = data.map(title => {
+        return {id: title._id, TitleName: title.TitleName, ReleaseYear: title.ReleaseYear, Participants: title.Participants };
+      });
+
+      this.titlesBSubject.next(Object.assign({}, this.titlesRepository).titles);
+      this.awardsBSubject.next(Object.assign({}, this.titlesRepository).awards);
+      this.storylinesBSubject.next(Object.assign({}, this.titlesRepository).storylines);
+      this.participantsBSubject.next(Object.assign({}, this.titlesRepository).participants);
+    });
   }
 
   getTitleNames(): Promise<any[]> {
